6a0105f6d475e297d36ce65091f1334316cb1c92
Ostrowskii <vanessaostroski08@gmail.com>
2023-05-25 16:51:31 -0400

    function added

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,10 +2,14 @@ function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   items = [];
 
+  const getMessage = (name: string) => {
+    return items.length === 0 ? <p>No item found, sra {name}</p> : null;
+  };
+
   return (
     <>
       <h1>List</h1>
-      {items.length === 0 ? <p>No item found</p> : null}
+      {getMessage("ostrowski")}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
