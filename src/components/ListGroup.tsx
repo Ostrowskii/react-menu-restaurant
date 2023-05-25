26d76eb0446e9e3bb62836deb77bf20b8ab3fd3e
Ostrowskii <vanessaostroski08@gmail.com>
2023-05-25 17:22:03 -0400

    selection added

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,36 @@
+import { MouseEvent, useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
+  // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   const getMessage = (name: string) => {
-    return items.length === 0 ? <p>No item found, sra {name}</p> : null;
+    return items.length === 0 && <p>No item found, sra {name}</p>;
   };
+
+  //Event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
       {getMessage("ostrowski")}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
