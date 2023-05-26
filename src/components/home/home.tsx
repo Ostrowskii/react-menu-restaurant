7e4bbdf8627f7cb228edcd5f8c59e972fc05472f
Ostrowskii <vanessaostroski08@gmail.com>
2023-05-26 18:26:13 -0400

    LowerMenu added into components

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,4 +1,10 @@
+import LowerMenu from "../shared/lowerMenu";
 function Home() {
-  return <h1>Bem vindo ao seu app de cardápio!</h1>;
+  return (
+    <>
+      <h1>Bem vindo ao seu app de cardápio!</h1>
+      <LowerMenu></LowerMenu>
+    </>
+  );
 }
 export default Home;
