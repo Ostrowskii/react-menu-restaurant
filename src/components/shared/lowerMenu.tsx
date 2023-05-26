e65d64710665c4cf5b054c0761348e6a35259bce
Ostrowskii <vanessaostroski08@gmail.com>
2023-05-26 18:19:27 -0400

    LowerMenu added

--- src/components/shared/lowerMenu.tsx
+++ src/components/shared/lowerMenu.tsx
@@ -1,29 +1,40 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+import HomeSvg from "../../assets/home.svg";
+import MenuSvg from "../../assets/menu.svg";
+import OrderSvg from "../../assets/order.svg";
 
 function LowerMenu() {
+  // function AcessHome() {
+  //   const navigate = useNavigate();
+  //   navigate("/");
+  // }
   function AcessMenu() {
     const navigate = useNavigate();
-    navigate("/teste");
+    navigate("/menu");
   }
+
   return (
     <div
       className="btn-group d-flex justify-content-end fixed-bottom"
       role="group"
       aria-label="Basic example"
     >
-      <button
-        onClick={AcessMenu}
-        type="button"
-        className="btn btn-primary btn-lg"
-      >
-        <img src="../../assets/home.svg" alt="home" />
-      </button>
-      <button type="button" className="btn btn-primary btn-lg">
-        menu
-      </button>
-      <button type="button" className="btn btn-primary btn-lg">
-        order
-      </button>
+      <Link to="/">
+        <button type="button" className="btn btn-primary btn-lg">
+          <img src={HomeSvg} alt="home" />
+        </button>
+      </Link>
+      <Link to="/menu">
+        <button type="button" className="btn btn-primary btn-lg">
+          <img src={MenuSvg} alt="menu" />
+        </button>
+      </Link>
+      <Link to="/order">
+        <button type="button" className="btn btn-primary btn-lg">
+          <img src={OrderSvg} alt="order" />
+        </button>
+      </Link>
     </div>
   );
 }
