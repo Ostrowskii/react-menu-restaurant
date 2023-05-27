89265b27e6dea78890c5f7199ce59899965fd327
Ostrowskii <vanessaostroski08@gmail.com>
2023-05-26 20:40:19 -0400

    list added

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,10 +1,42 @@
 import LowerMenu from "../shared/lowerMenu";
 
 function Menu() {
+  const menuItems = [
+    { id: 1, name: "Salada vegetariana", price: 5.99 },
+    { id: 2, name: "Hambúrguer com queijo", price: 7.99 },
+    { id: 3, name: "Pizza de margherita", price: 9.99 },
+  ];
+
   return (
     <>
       <h1>menu</h1>
-      <LowerMenu></LowerMenu>;
+      <ul className="list-group">
+        {menuItems.map((item) => (
+          <li className="list-group-item" key={item.id}>
+            <div className="row">
+              <div className="col-3">
+                <input
+                  type="number"
+                  className="form-control"
+                  id={`quantityInput-${item.id}`}
+                />
+              </div>
+              <div className="col-2">
+                <label htmlFor={`quantityInput-${item.id}`}>{item.price}</label>
+              </div>
+              <div className="col-7">
+                <label
+                  className="form-check-label"
+                  htmlFor={`quantityInput-${item.id}`}
+                >
+                  {item.name}
+                </label>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <LowerMenu></LowerMenu>
     </>
   );
 }
--- src/components/order/order.tsx
+++ src/components/order/order.tsx
@@ -3,7 +3,8 @@ import LowerMenu from "../shared/lowerMenu";
 function Order() {
   return (
     <>
-      <h1>Order</h1>;<LowerMenu></LowerMenu>
+      <h1>Order</h1>
+      <LowerMenu></LowerMenu>
     </>
   );
 }
